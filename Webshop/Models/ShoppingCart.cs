c4ab2418490860565a5c111bc36899c46a445bd5
11Larsson11 <richard.auerhahn@gmail.com>
2022-03-28 18:10:00 +0200

    Constructed a list of products

---
+++ Webshop/Models/ShoppingCart.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webshop.Models
+{
+    public class ShoppingCart
+    {
+    }
+}
---
+++ Webshop/Viewmodels/ProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webshop.Models;
+
+namespace Webshop.Viewmodels
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public Category Category { get; set; }
+
+        public ProductViewModel() { }
+
+    }
+}
