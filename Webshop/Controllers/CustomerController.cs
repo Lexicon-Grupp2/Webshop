eb229279e6599355dff4269dc30de0f772510bf5
11Larsson11 <richard.auerhahn@gmail.com>
2022-04-14 12:18:22 +0200

    Added receipt functionality

--- Webshop/Controllers/CustomerController.cs
+++ Webshop/Controllers/CustomerController.cs
@@ -6,19 +6,51 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Webshop.Data;
+using Webshop.Models;
+using Webshop.Viewmodels;
 
 namespace Webshop.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Receipt()
+        
+        public IActionResult Receipt(int id)
         {
-            return View();
+            Order order = _context.Orders.FirstOrDefault(order => order.OrderId == id);
+
+            _context.OrderDetails.ToList();
+            List<OrderDetail> details = order.OrderDetails.ToList();
+
+            ApplicationUser user = _context.Users.FirstOrDefault(user => user.Id == order.CustomerId);
+
+            ReceiptViewModel receiptViewModel = new ReceiptViewModel()
+            {
+                OrderId = id,
+                OrderDate = order.OrderDate,
+                TotalCost = order.TotalCost,
+                CustomerId = order.CustomerId,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Address = user.Address,
+                PostalCode = user.PostalCode,
+                City = user.City,
+                Country = user.Country,
+                OrderDetails = details
+            };
+
+            return View("Receipt", receiptViewModel);
         }
 
 
---
+++ Webshop/Viewmodels/ReceiptViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webshop.Models;
+
+namespace Webshop.Viewmodels
+{
+    public class ReceiptViewModel
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string CustomerId { get; set; }
+        public int TotalCost { get; set; }
+
+    }
+}
