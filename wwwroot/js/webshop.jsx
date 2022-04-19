753d7affb96b2c2a9b9513756aeb82db909d157d
Neoleds <rikard@densloe.net>
2022-04-19 13:36:40 +0200

    Latest css update

---
+++ wwwroot/js/webshop.jsx
@@ -0,0 +1,14 @@
+﻿class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">
+
+                        <div>
+                            <p>Webshop King is under construction.</p>
+                        </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<CommentBox />, document.getElementById('content'));
